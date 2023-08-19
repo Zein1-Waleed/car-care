a1ab4e5bc2e54a212bc1f8e5317faea8302f8373
Zein1-Waleeed <wzein36@gmail.com>
2023-08-20 01:17:33 +0400

    make service page

--- main.js
+++ main.js
@@ -10,37 +10,3 @@ menu.addEventListener("click",()=>{
     navlist.classList.toggle("active")
 })
 
-const translations = {
-    en: {
-        welcome: "Welcome To Reflection",
-        isQuality: "Reflection is a quality of you",
-        // ... add more translations
-    },
-    ar: {
-        welcome: "مرحبًا بك في ريفليكشن",
-        isQuality: "الانعكاس هو جودة تتميز بها",
-        // ... add more translations
-    }
-    // ... add translations for more languages
-};
-
-function updateContent(language) {
-    const elements = document.querySelectorAll('[data-translation-key]');
-    
-    elements.forEach(element => {
-        const translationKey = element.getAttribute('data-translation-key');
-        if (translations[language] && translations[language][translationKey]) {
-            element.textContent = translations[language][translationKey];
-        }
-    });
-}
-
-let currentLanguage = 'en'; // Default language
-
-function toggleLanguage() {
-    currentLanguage = (currentLanguage === 'en') ? 'ar' : 'en';
-    updateContent(currentLanguage);
-}
-
-// Call initial translation
-updateContent(currentLanguage);
