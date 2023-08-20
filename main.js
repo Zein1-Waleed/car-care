ac7b682c6deaf56da8eaba44be3920a8468cff05
Zein1-Waleeed <wzein36@gmail.com>
2023-08-20 23:41:52 +0400

    last update

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ menu.addEventListener("click",()=>{
     navlist.classList.toggle("active")
 })
 
+let modalIcon = document.querySelector("#modal-icon h3");
+let modal = document.getElementById("modal")
+let modalImg = document.querySelector(".modal img")
+let certifimages = document.getElementById("cetifimages").children;
+
+
+for(let i = 0; i < certifimages.length;i++){
+    certifimages[i].addEventListener("click",(e)=>{
+        modalImg.src = e.target.src
+        modal.classList.add('active')
+    })
+}
+
+
+modalIcon.addEventListener("click",()=>{
+    modal.classList.remove("active")
+})
